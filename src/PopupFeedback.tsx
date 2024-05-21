9e36493516350b7742b4f67d56a82bd50f4c2aff
FagianoRampante <56256292+surrend-15min@users.noreply.github.com>
2024-05-21 12:33:07 +0200

    Fixed Popup

--- src/PopupFeedback.tsx
+++ src/PopupFeedback.tsx
@@ -1,42 +1,36 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import './PopupFeedback.css';
 
 const PopupFeedback: React.FC = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const [inputValue, setInputValue] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const [inputValue, setInputValue] = useState('');
 
-    const handleButtonClick = () => {
-        closeAllPopups();
-        setIsOpen(true);
-    };
+  const handleButtonClick = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const handleCloseButtonClick = () => {
-        setIsOpen(false);
-    };
+  const handleCloseButtonClick = () => {
+    setIsOpen(false);
+  };
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setInputValue(event.target.value);
-    };
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
 
-    const closeAllPopups = () => {
-        const popups = document.querySelectorAll('.popup');
-        popups.forEach((popup) => {
-            (popup as HTMLElement).style.display = 'none';
-        });
-    };
-
-    return (
-        <div>
-            <button onClick={handleButtonClick}>Invia feedback</button>
-            {isOpen && (
-                <div className="popup">
-                    <h2>Titolo del Popup</h2>
-                    <input type="text" value={inputValue} onChange={handleInputChange} />
-                    <button className="close-popup" onClick={handleCloseButtonClick}>Chiudi</button>
-                </div>
-            )}
-        </div>
-    );
+  return (
+    <div>
+      <button onClick={handleButtonClick}>Invia feedback</button>
+      {isOpen && ReactDOM.createPortal(
+        <div className="popup">
+          <h2>Titolo del Popup</h2>
+          <input type="text" value={inputValue} onChange={handleInputChange} />
+          <button className="close-popup" onClick={handleCloseButtonClick}>Chiudi</button>
+        </div>,
+        document.body
+      )}
+    </div>
+  );
 };
 
 export default PopupFeedback;
